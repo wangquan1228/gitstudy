5a94b972d20965432b1e295f0177f5ab07c889d2
wangquan <wangquan@163.com>
2020-03-01 02:01:07 +0800

    master添加

--- springboot-test/src/main/java/com/wq/springboot/controller/ImageTestController.java
+++ springboot-test/src/main/java/com/wq/springboot/controller/ImageTestController.java
@@ -38,7 +38,7 @@ import java.math.BigDecimal;
         System.out.println(s);
 
         System.out.println("21343");
-        System.out.println("mastertest");
+        System.out.println("mastert儿我热无若est");
 
      /*   String str="123";
         Object o=null;
